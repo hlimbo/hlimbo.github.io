3a70755807d4c9dc2f6350e2b99f0a2155bf06cd
hlimbo <hlimbo@uci.edu>
2020-01-13 23:46:53 -0800

    Updates

---
+++ precache-manifest.6783e68156f57581f7eeb308b7470ee5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b715ea554c2b8e8ad1e766c79fff2cb0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4a54315665e7ec1550c8",
+    "url": "/static/css/main.8ab6f918.chunk.css"
+  },
+  {
+    "revision": "3431ede86ad33120cb0f",
+    "url": "/static/js/2.ac261752.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.ac261752.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4a54315665e7ec1550c8",
+    "url": "/static/js/main.2713060a.chunk.js"
+  },
+  {
+    "revision": "cd26e72069df6a56a4bd",
+    "url": "/static/js/runtime-main.b5207b5b.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  }
+]);
