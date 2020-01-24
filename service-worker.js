b3e66f35c62bf80a3f1b447ba92445b7df78feb2
hlimbo <hlimbo@uci.edu>
2020-01-23 17:13:13 -0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.24882ccadda09ed4fb1a10ddd61902c2.js"
+  "/precache-manifest.828d29bcdeb763126218425e1bcbff38.js"
 );
 
 self.addEventListener('message', (event) => {
