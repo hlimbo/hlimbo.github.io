16f4f9ac0d9d94677bca26d4c08d024bfb0b069f
hlimbo <hlimbo@uci.edu>
2020-01-23 23:17:37 -0800

    add lots of things (scrolling to correct section updates tab highlight, added MathNinja, added responsive view to unity game containers, added animations to about me blurb)

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import "animate.css";
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 import AppBar from '@material-ui/core/AppBar';
@@ -9,6 +10,28 @@ import Paper from '@material-ui/core/Paper';
 import { createMuiTheme } from '@material-ui/core/styles';
 import HomePage from './HomePageWIP';
 import Unity, { UnityContent } from 'react-unity-webgl';
+import { Typography } from '@material-ui/core';
+import Box from '@material-ui/core/Box';
+import Grid from '@material-ui/core/Grid';
+// add this inside of webpack.config.js located inside of node_modules directory
+/*
+        // ttf loader
+        {
+          test: /\.ttf$/,
+          use: [
+            {
+              loader: 'ttf-loader',
+              options: {
+                name: './font/[hash].[ext]',
+              },
+            },
+          ]
+        }
+*/
+// Adding text is complicated af must inject a dependency into webpack but since i'm using create-react-app I can't modify webpack.config.js directly
+// import Ubuntu from './fonts/Ubuntu/Ubuntu.ttf';
+
+import 'typeface-roboto';
 
 
 const theme = createMuiTheme({
@@ -25,6 +48,79 @@ const theme = createMuiTheme({
 // Container Component (Centers content horizontally)
 // Does this work with react?
 // https://www.npmjs.com/package/react-unity-webgl <- it works like magic :)
+function Game() {    
+  return (
+    <Paper>
+      <Container>
+        <h2>MathNinja</h2>
+        <Unity unityContent={this.unityContent} className="game-container"/>
+      </Container>
+      <section>
+        <h1>How to Play</h1>
+        <p>Use arrow keys to move. Spacebar to Jump.</p>
+      </section>
+      <section class="asset_credits">
+        <h1>Asset Credits:</h1>
+        <p><strong>Note: </strong> All art assets used in the game are placeholders. </p>
+        <p>
+          <ul>
+            <li>Ninja (Spelunky) <a href="https://www.spriters-resource.com/fullview/56425/">https://www.spriters-resource.com/fullview/56425/</a></li>
+            <li>tiles and background images by Franco Giachetti (<a href="http://www.ludicarts.com/free-game-platform-tileset/">http://www.ludicarts.com/free-game-platform-tileset/</a>) which is licensed under <a href="https://creativecommons.org/licenses/by/4.0/">Creative Commons Attribution 4.0 International License</a></li>
+          </ul>
+        </p>
+      </section>
+    </Paper>
+  );
+}
+
+function animateCSS(element, animationName, callback) {
+  const node = document.querySelector(element)
+  node.classList.add('animated', animationName)
+
+  function handleAnimationEnd() {
+      node.classList.remove('animated', animationName)
+      node.removeEventListener('animationend', handleAnimationEnd)
+
+      if (typeof callback === 'function') callback()
+  }
+
+  node.addEventListener('animationend', handleAnimationEnd)
+}
+
+const names = [ 'a Software Engineer', 'a UCI Graduate', 'Harvey Limbo'];
+
+function IntroCard() {
+  const [nameTagIndex, setNameTagIndex] = React.useState(0);
+
+  // only gets called after all dom mutations happen
+  React.useLayoutEffect(() => {
+    animateCSS('.nameTag', 'slideInDown', () => {
+      if(nameTagIndex < names.length - 1) {
+        setTimeout(() => setNameTagIndex(nameTagIndex + 1), 1000);
+      }
+    });
+  });
+
+  return (
+    <Container maxWidth="sm" align='center'>
+      <Grid container spacing={2}>
+        <Grid item>
+          <Typography variant="h2">Hi I'm...</Typography>
+        </Grid>
+        <Grid item>
+          <Paper square variant="outlined" style={{minWidth: "50vw"}}>
+            <Typography align="center" 
+                        variant="h2"
+                        color="primary" 
+                        className="nameTag">
+                          {names[nameTagIndex]}
+            </Typography>
+          </Paper>
+        </Grid>
+      </Grid>
+    </Container>
+  );
+}
 
 class App extends React.Component {
   constructor(){
@@ -38,28 +134,12 @@ class App extends React.Component {
 
   render() {
     return (
-      <Paper>
-        <Container maxWidth="md">
-          <h2>MathNinja</h2>
-          <Unity unityContent={this.unityContent} className="game-container"/>
-        </Container>
-        <section>
-          <h1>How to Play</h1>
-          <p>Use arrow keys to move. Spacebar to Jump.</p>
-        </section>
-        <section class="asset_credits">
-          <h1>Asset Credits:</h1>
-          <p><strong>Note: </strong> All art assets used in the game are placeholders. </p>
-          <p>
-            <ul>
-              <li>Ninja (Spelunky) <a href="https://www.spriters-resource.com/fullview/56425/">https://www.spriters-resource.com/fullview/56425/</a></li>
-              <li>tiles and background images by Franco Giachetti (<a href="http://www.ludicarts.com/free-game-platform-tileset/">http://www.ludicarts.com/free-game-platform-tileset/</a>) which is licensed under <a href="https://creativecommons.org/licenses/by/4.0/">Creative Commons Attribution 4.0 International License</a></li>
-            </ul>
-          </p>
-        </section>
-      </Paper>
+      <div>
+        <HomePage>
+          <IntroCard/>
+        </HomePage>
+      </div>
     );
-    //return <HomePage/>;
   }
 }
 
--- src/HomePageWIP.js
+++ src/HomePageWIP.js
@@ -6,29 +6,80 @@ import './App.css';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 import AppBar from '@material-ui/core/AppBar';
+import Typography from '@material-ui/core/Typography';
+import Paper from '@material-ui/core/Paper';
+import Container from '@material-ui/core/Container';
 import { createMuiTheme } from '@material-ui/core/styles';
+import scrollIntoView from 'scroll-into-view';
 
-// bad solution need to get a better one... maybe use react router?
-function scrollToId(id) {
-  const el = document.getElementById(id); 
-  el.scrollIntoView();
+// credits: https://gist.github.com/gre/1650294
+const easeOutQuad = t => t*(2-t);
+
+function scrollEaseOutById(id) {
+  scrollIntoView(
+    document.getElementById(id),
+   { ease: easeOutQuad });
 }
 
-function HomePage() {
+function isInViewportView(boundingBox) {
+  const viewportWidth = (window.innerWidth || document.documentElement.clientWidth);
+  const viewportHeight = (window.innerHeight || document.documentElement.clientHeight);
+  return (boundingBox.top >= 0 && 
+    boundingBox.left >= 0 && 
+    boundingBox.right <= viewportWidth && 
+    boundingBox.bottom <= viewportHeight);
+}
+
+let scrollEventAdded = false;
+function HomePage(props) {
+  // value here serves as the index for the tabs
   const [value, setValue] = React.useState(0);
-  console.log("The value: ", value);
+
+  React.useEffect(() => {
+    if(!scrollEventAdded) {
+      scrollEventAdded = true;
+      window.addEventListener("scroll", () => { 
+        const aboutMe = document.querySelector("#about_me");
+        const projects = document.querySelector("#projects");
+        const aboutMeBoundBox = aboutMe.getBoundingClientRect();
+        const projectsBoundBox = projects.getBoundingClientRect();
+        if(isInViewportView(aboutMeBoundBox)) {
+          setValue(0);
+        } else if(isInViewportView(projectsBoundBox)) {
+          setValue(1);
+        }
+      });
+    }
+  });
+  
   return(
     <div>
       <AppBar position="sticky" color="primary">
         <Tabs 
           centered={true}
           value={value} 
-          onChange={(event, newValue) => { setValue(newValue); }}>
-          <Tab label="About Me" disableRipple={true} onClick={ () => scrollToId('about_me') }/>
-          <Tab label="Projects" disableRipple={true} onClick={ () => scrollToId('projects') }/>
+          onChange={(event, newValue) => { console.log('newValue: ',newValue); setValue(newValue); }}>
+          <Tab label="About Me" 
+               disableRipple 
+               onClick={() => { scrollEaseOutById("about_me"); setValue(0); }}/>
+          <Tab label="Projects" 
+               disableRipple 
+               onClick={ () => { scrollEaseOutById("projects"); setValue(1); } }/>
         </Tabs>
       </AppBar>
-      <div id="about_me">About Me </div>
+      <br/>
+      <div id="about_me">
+        {props.children}
+        <br/>
+        <br/>
+        <Container maxWidth="md">
+          <Paper elevation={0}>
+            <Typography>
+              I got my first exposure to programming at my high school's AP Comp Sci Course and since then I have gained a deep interest in building software to entertain and help people. Below are my projects I've done during my spare time, game jams, and school projects:
+            </Typography>
+          </Paper>
+        </Container>
+      </div>
       <div>alfkjalfjaljfalfjlajfla </div>
       <div>alfkjalfjaljfalfjlajfla </div>
       <div>alfkjalfjaljfalfjlajfla </div>
@@ -61,6 +112,9 @@ function HomePage() {
       <div>alfkjalfjaljfalfjlajfla </div>
       <div>alfkjalfjaljfalfjlajfla </div>
       <div>alfkjalfjaljfalfjlajfla </div>
+      <Container maxWidth="md" id="projects">
+        <Typography variant="h2">Projects</Typography>
+      </Container>
       <div>alfkjalfjaljfalfjlajfla </div>
       <div>alfkjalfjaljfalfjlajfla </div>
       <div>alfkjalfjaljfalfjlajfla </div>
@@ -79,7 +133,12 @@ function HomePage() {
       <div>alfkjalfjaljfalfjlajfla </div>
       <div>alfkjalfjaljfalfjlajfla </div>
       <div>alfkjalfjaljfalfjlajfla </div>
-      <div id="projects">Projects</div>
+      <div>alfkjalfjaljfalfjlajfla </div>
+      <div>alfkjalfjaljfalfjlajfla </div>
+      <div>alfkjalfjaljfalfjlajfla </div>
+      <div>alfkjalfjaljfalfjlajfla </div>
+      <div>alfkjalfjaljfalfjlajfla </div>
+      <div>alfkjalfjaljfalfjlajfla </div>
       <div>alfkjalfjaljfalfjlajfla </div>
       <div>alfkjalfjaljfalfjlajfla </div>
       <div>alfkjalfjaljfalfjlajfla </div>
